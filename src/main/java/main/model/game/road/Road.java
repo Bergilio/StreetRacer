1c06c12cd933246e4b801a5797869535b5cc5583
Pedro Salgado <pedrosalgado7704@gmail.com>
2024-11-25 00:52:19 +0000

    Updated something

--- src/main/java/main/model/game/road/Road.java
+++ src/main/java/main/model/game/road/Road.java
@@ -6,6 +6,7 @@ import main.model.game.elements.Obstacle;
 import main.model.game.elements.PlayerCar;
 import main.model.game.elements.RoadLimit;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Road {
@@ -14,13 +15,38 @@ public class Road {
 
     private PlayerCar playerCar = new PlayerCar(10, 18, 20);
 
-    private List<Obstacle> obstacles;
-    private List<Fuel> fuels;
-    private List<RoadLimit> limits;
+    private List<Obstacle> obstacles = createObstacles();
+    private List<Fuel> fuels = createFuels();
+    private List<RoadLimit> limits = createLimits();
 
     public Road(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    public List<Obstacle> createObstacles() {
+        List<Obstacle> obstacles = new ArrayList<>();
+        obstacles.add(new Obstacle(3, 19));
+        obstacles.add(new Obstacle(10, 14));
+        obstacles.add(new Obstacle(3, 6));
+        return obstacles;
+    }
+
+    public List<Fuel> createFuels() {
+        List<Fuel> fuels = new ArrayList<>();
+        fuels.add(new Fuel(17, 19));
+        fuels.add(new Fuel(3, 14));
+        fuels.add(new Fuel(17, 6));
+        return fuels;
+    }
+
+    public List<RoadLimit> createLimits() {
+        List<RoadLimit> limits = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            limits.add(new RoadLimit(i, 0));
+            limits.add(new RoadLimit(i, 19));
+        }
+        return limits;
     }
 
     public boolean isLimit(Position position) {
